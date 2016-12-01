0cf376545bc12e2c8c4b51b467b1bf2d8ef2ab84
Jannis R <mail@jannisr.de>
2016-12-01 01:05:15 +0100

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 'use strict'
 
-// todo
+const onPrint = require('window-on-print')
+
+const print = (html, cb, delay) => {
+	const iframe = document.createElement('iframe')
+	iframe.setAttribute('sandbox', 'allow-modals allow-same-origin allow-scripts')
+	iframe.setAttribute('src', '/print.html')
+	iframe.setAttribute('style', 'display: none')
+
+	document.body.appendChild(iframe)
+	const teardown = () => {
+		// Safari crashes without a timeout.
+		setTimeout(() => document.body.removeChild(iframe), 0)
+		if (typeof cb === 'function') cb()
+	}
+
+	setTimeout(() => {
+		iframe.contentDocument.write(html)
+
+		setTimeout(() => {
+			onPrint.after(iframe.contentWindow, teardown)
+			iframe.contentWindow.focus()
+			iframe.contentWindow.print()
+		}, delay || 50)
+	}, 0)
+}
+
+module.exports = print
